e3fbe747dd3cbf1eeff652b89940a189c633d934
cvolpe12 <christopher.volpe12@gmail.com>
2019-11-21 13:27:19 -0500

    adding new models

--- AppTwo/admin.py
+++ AppTwo/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from AppTwo.models import User
+from AppTwo.models import User, School, Student
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(School)
+admin.site.register(Student)
--- AppTwo/models.py
+++ AppTwo/models.py
@@ -9,3 +9,19 @@ class User(models.Model):
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
+
+class School(models.Model):
+    name = models.CharField(max_length=264)
+    principal = models.CharField(max_length=264)
+    location = models.CharField(max_length=264)
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    name = models.CharField(max_length=264)
+    age = models.PositiveIntegerField()
+    school = models.ForeignKey(School, related_name='students', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
